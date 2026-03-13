765316fa153332a5f6e881c7fc016f1d1e9af9f9
mugiwaraventures <tramposon12345@gmail.com>
2026-03-13 10:06:55 +0000

    fix: corrigindo importação de lições no painel principal

--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { STAGES } from '../lib/lessons';
+import { STAGES } from '../../lib/lessons';
 
 const premiumMaterials = [
   {
